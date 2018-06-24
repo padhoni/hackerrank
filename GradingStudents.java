fe06a64030a13b64e691ab5063bb9456d505fba8
padhoni <ps.dhoni@gmail.com>
2018-06-24 10:34:34 -0700

    StudentsGrading Program
    StudentsGrading Program

---
+++ GradingStudents.java
@@ -0,0 +1,62 @@
+package hckrrank;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class GradingStudents {
+
+    /*
+     * Complete the gradingStudents function below.
+     */
+    static int[] gradingStudents(int[] grades) {
+        /*
+         * Write your code here.
+         */
+        int rem;
+		int diff;
+        int len=grades.length;
+		for (int i=0;i<len;i++)
+		{
+			rem=grades[i]%5;
+			diff=5-rem;
+			
+			if(diff<3 && grades[i]>35)
+			{
+				grades[i] = grades[i]+diff;
+			}
+		}
+		return grades;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] grades = new int[n];
+
+        for (int gradesItr = 0; gradesItr < n; gradesItr++) {
+            int gradesItem = Integer.parseInt(scan.nextLine().trim());
+            grades[gradesItr] = gradesItem;
+        }
+
+        int[] result = gradingStudents(grades);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write("\n");
+            }
+        }
+
+        bw.newLine();
+
+        bw.close();
+    }
+}
